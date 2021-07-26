a1ca13db609fb2b331e34cf3a1cf7fd120a2bd16
Crypto-Path <60126762+Crypto-Path@users.noreply.github.com>
2021-07-26 16:58:48 -0700

    Add files via upload

---
+++ Morania Page/Beastionary/Morania-Beastionary-Images.js
@@ -0,0 +1,5 @@
+var DefaultImage = "Beastionary/Images/Default.jpg";
+var ForrestSlime = "Beastionary/Images/Slime-Grass.jpg";
+var EarthSlime = "Beastionary/Images/Slime-Earth.jpg";
+var WaterSlime = "Beastionary/Images/Default.jpg";
+var FireSlime = "Beastionary/Images/Slime-Fire.jpg";
